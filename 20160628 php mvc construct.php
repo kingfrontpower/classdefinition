a9a5de5ae6a087ff6d1a12ba7731ffa17e1d4c47
chuan <kingfrontpower@gmail.com>
2016-06-28 14:06:38 +0800

    原始版

--- 20160628 php mvc construct.php
+++ 20160628 php mvc construct.php
@@ -14,35 +14,8 @@
 
         class Cat 
         {
-            public $name;
-            protected $position;
-
-            public function __construct($name) //方法,建構子
-                    {
-                $this->name = $name;
-                $this->position = ['x'=>0, 'y'=>0];
-            }
-            public function moveTo($x, $y){
-                $this->position['x'] = $x;
-                $this->position['y'] = $y;
-                return $this->position;
-            }
-            public function resetPosition()
-            {
-                $this->moveTo(0,0);
-                return $this->position;
-            }  
         }
-        $pet = new Cat("Kitty"); 
-        echo $pet->name;
-        echo "<br>";
-        $pet->name = "Hello Kitty";
-        echo $pet->name;
-        echo "<br>";
         
-        $movePosition = $pet->moveTo(3,5);
-        echo "(".$movePosition['x'].",".$movePosition['y'].")";
-        echo "<br>";
         ?>
     </body>	  
 </html>
