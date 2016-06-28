9d074551b0fab3cf482f77f2a6bab7cfd742127a
chuan <kingfrontpower@gmail.com>
2016-06-28 13:53:42 +0800

    註解版

---
+++ 20160628 php mvc construct.php
@@ -0,0 +1,48 @@
+<!doctype html><!--宣告文件類型-->
+<html lang="zh-tw"><!--指定網頁使用語言-->
+    <head><!--文件的標頭-->
+        <meta charset="utf-8"><!--指定網頁的編碼-->
+        <!--
+        To change this license header. choose License Headers in Project Properties.
+        To change this template file, choose Tools/ templates 
+        and open the template in the editor .
+        -->
+        <title>mvc consturct </title><!--文件標題-->      
+    </head>
+    <body> <!--文件主體-->
+        <?php
+
+//        class Cat 
+//        {
+//            public $name;
+//            protected $position;
+//
+//            public function __construct($name) //方法,建構子
+//                    {
+//                $this->name = $name;
+//                $this->position = ['x'=>0, 'y'=>0];
+//            }
+//            public function moveTo($x, $y){
+//                $this->position['x'] = $x;
+//                $this->position['y'] = $y;
+//                return $this->position;
+//            }
+//            public function resetPosition()
+//            {
+//                $this->moveTo(0,0);
+//                return $this->position;
+//            }  
+//        }
+//        $pet = new Cat("Kitty"); 
+//        echo $pet->name;
+//        echo "<br>";
+//        $pet->name = "Hello Kitty";
+//        echo $pet->name;
+//        echo "<br>";
+//        
+//        $movePosition = $pet->moveTo(3,5);
+//        echo "(".$movePosition['x'].",".$movePosition['y'].")";
+//        echo "<br>";
+        ?>
+    </body>	  
+</html>
