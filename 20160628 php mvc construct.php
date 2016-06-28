7fd0fabdd524ad980632f139054ef37453fd5c7c
chuan <kingfrontpower@gmail.com>
2016-06-28 15:38:58 +0800

    setName getName

--- 20160628 php mvc construct.php
+++ 20160628 php mvc construct.php
@@ -12,36 +12,56 @@
     <body> <!--文件主體-->
         <?php
 
-        class Cat 
-        {
-            public $name;
+        class Cat {
+
+//            public $name;
+            protected $name;
             protected $position;
 
-            public function __construct($name) //方法,建構子
-                    {
+            public function __construct($name) { //方法,建構子
                 $this->name = $name;
-                $this->position = ['x'=>0, 'y'=>0];
+                $this->position = ['x' => 0, 'y' => 0];
             }
-            public function moveTo($x, $y){
+
+            public function setName($name) {
+                $this->name = $name;
+            }
+            public function gettName($name) {
+                return $this->name;
+            }
+
+            public function moveTo($x, $y) {
                 $this->position['x'] = $x;
                 $this->position['y'] = $y;
                 return $this->position;
             }
-            public function resetPosition()
-            {
-                $this->moveTo(0,0);
+
+            public function move($x, $y) {
+                $this->position['x'] += $x;
+                $this->position['y'] += $y;
                 return $this->position;
-            }  
+            }
+
+            public function resetPosition() {
+                $this->moveTo(0, 0);
+                return $this->position;
+            }
+
         }
-        $pet = new Cat("Kitty"); 
+
+        $pet = new Cat("Kitty");
         echo $pet->name;
         echo "<br>";
         $pet->name = "Hello Kitty";
         echo $pet->name;
         echo "<br>";
-        
-        $movePosition = $pet->moveTo(3,5);
-        echo "(".$movePosition['x'].",".$movePosition['y'].")";
+
+        $movePosition = $pet->moveTo(3, 5);
+        echo "(" . $movePosition['x'] . "," . $movePosition['y'] . ")";
+        echo "<br>";
+
+        $movePosition = $pet->move(5, 3);
+        echo "(" . $movePosition['x'] . "," . $movePosition['y'] . ")";
         echo "<br>";
         ?>
     </body>	  
